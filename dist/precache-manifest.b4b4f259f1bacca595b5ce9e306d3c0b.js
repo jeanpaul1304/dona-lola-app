c64f94ed35c2a3bac6f4acff3c3eeeadbd923e1b
Jean Paul Diaz Vivanco <jdiazvi2@intercorp.com.pe>
2018-10-24 17:16:14 -0500

    up dist

---
+++ dist/precache-manifest.b4b4f259f1bacca595b5ce9e306d3c0b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "cd4a168755af8a25c811",
+    "url": "/js/chunk-vendors.698fe423.js"
+  },
+  {
+    "revision": "357448450f5989e9b1cc",
+    "url": "/js/app.6b809092.js"
+  },
+  {
+    "revision": "d2a5a3958a4677960dd9",
+    "url": "/js/about.72f7e6cd.js"
+  },
+  {
+    "revision": "f524decc8f098800aef7ab2162945eb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7f4582799181f0e4d41a996a991f25d",
+    "url": "/img/logo.b7f45827.png"
+  },
+  {
+    "revision": "662c51cc73d3463b72b62f6d6a185ad1",
+    "url": "/img/bg.662c51cc.jpg"
+  },
+  {
+    "revision": "cd4a168755af8a25c811",
+    "url": "/css/chunk-vendors.5cf92f0a.css"
+  },
+  {
+    "revision": "357448450f5989e9b1cc",
+    "url": "/css/app.bc7f9dd0.css"
+  }
+];
